4deb63fdd411c14a97e43531a0563eca45341c25
Yar-nikita <74370771+Yar-nikita@users.noreply.github.com>
2021-11-02 14:55:45 +0500

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:2
+        },
+       
+        1300:{
+            items:2
+        },
+        1400:{
+            items:3
+        }
+    }
+})
